30c050fcdf2cef6ed65bc3a730878c2b09d44177
zerahhah <zerahhah@gmail.com>
2018-01-15 22:04:45 +0800

    6th

--- mysite06/settings.py
+++ mysite06/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ubuntu/mysite06/stc'
+STATIC_ROOT = '/home/ubuntu/blog/stc'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
